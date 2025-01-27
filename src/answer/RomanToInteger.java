d0bb8d74ba4831f1040ed02ad50b270fe910b0ba
asa129 <asa.jad1290@gmail.com>
2025-01-27 22:19:51 +0900

    feat: :sparkles: #13追加

---
+++ src/answer/RomanToInteger.java
@@ -0,0 +1,30 @@
+package answer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RomanToInteger {
+    public int romanToInt(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int answer = 0;
+        int n = s.length();
+
+        for(int i = 0; i < n; i++){
+            if(i < n - 1 && map.get(s.charAt(i)) < map.get(s.charAt(i + 1))){
+                answer += map.get(s.charAt(i + 1)) - map.get(s.charAt(i));
+                i++;
+            }else{
+                answer += map.get(s.charAt(i));
+            }
+        }
+        return answer;
+    }
+}
