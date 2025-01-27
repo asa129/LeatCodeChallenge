8504042c71c49616697e3866017d03827fba45c0
asa129 <asa.jad1290@gmail.com>
2025-01-27 22:20:09 +0900

    test: :green_heart: #13テストコード追加

---
+++ src/test/RomanToIntegerTest.java
@@ -0,0 +1,31 @@
+package test;
+
+import answer.RomanToInteger;
+import answer.TwoSum;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class RomanToIntegerTest {
+
+    RomanToInteger romanToInteger = new RomanToInteger();
+
+    @Test
+    void romanToInt() {
+        String s = "III";
+        assertEquals(3, romanToInteger.romanToInt(s));
+    }
+
+    @Test
+    void romanToInt2() {
+        String s = "LVIII";
+        assertEquals(58, romanToInteger.romanToInt(s));
+    }
+
+    @Test
+    void romanToInt3() {
+        String s = "MCMXCIV";
+        assertEquals(1994, romanToInteger.romanToInt(s));
+    }
+}
